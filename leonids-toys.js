406563f3c6f3a0b9c92c8bfee31fbb40fe7555e3
tbronny <tristan.bron7@gmail.com>
2021-06-01 18:33:00 -0500

    initial commit

---
+++ leonids-toys.js
@@ -0,0 +1,26 @@
+const dolls = [
+    {
+        id: 1,
+        name: 'Kendall',
+        maker: 'americanGirl',
+        type: 'WellieWishers',
+        soldBy: 'amazon',
+        price: 24,
+    },
+    {
+        id: 2,
+        name: 'Samantha 1905',
+        maker: 'americanGirl',
+        type: 'historical characters',
+        soldBy: 'ebay',
+        price: 150,
+    },
+    {
+        id: 3,
+        name: 'Courtney 1986',
+        maker: 'americanGirl',
+        type: 'historical characters',
+        soldBy: 'aliexpress',
+        price: 110,
+    },
+];
